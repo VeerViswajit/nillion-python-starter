0247da622afb06df99b28391bf5d791bbde29258
VeerViswajit <veerviswajit2004@gmail.com>
2024-06-29 22:45:04 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,21 +1,31 @@
 from nada_dsl import *
 
-
 def nada_main():
     party1 = Party(name="Party1")
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
-    
-    r=my_int1+my_int2
-    r2=my_int1-my_int2
-    # write the computation for your program here - use my_int1 and my_int2 as inputs
-    # make sure you change the output below to be your new output
-    out1=Output(r,"addtion",party1)
-    out2=Output(r2,"subdtraction",party1)
-    r3=my_int1-Integer(496)
-    r4=my_int2-Integer(1)
-    r5=Integer(0)
-    out3=Output(r3,"Start Date: ",party1)
-    out4=Output(r4,"End Date: ",party1)
-    out5=Output(r5,"Welcome to Hacker House 2024 GOA",party1)
-    return [out1,out2,out3,out4,out5]
+
+    r = my_int1 * my_int2
+    r3 = my_int1 / my_int2
+    r_mod = my_int1 % my_int2  # Modulo operation
+
+    # Function to find the maximum of two numbers
+    def max(a: SecretInteger, b: SecretInteger) -> SecretInteger:
+        return (a < b).if_else(b, a)
+
+    # Function to find the minimum of two numbers
+    def min(a: SecretInteger, b: SecretInteger) -> SecretInteger:
+        return (a < b).if_else(a, b)
+
+    r1 = max(my_int1, my_int2)
+    r6 = min(my_int1, my_int2)
+    r5 = Integer(0)
+
+    out1 = Output(r1, "Maximum of two Numbers", party1)
+    out2 = Output(r, "Multiplication of Two Numbers", party1)
+    out3 = Output(r3, "Division of Two Numbers", party1)
+    out4 = Output(r6, "Minimum of two Numbers", party1)
+    out5 = Output(r_mod, "Modulo of Two Numbers", party1)
+    out6 = Output(r5, "Welcome to Hacker House GOA", party1)
+
+    return [out1, out2, out3, out4, out5, out6]
