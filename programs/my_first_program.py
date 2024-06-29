b4b309ff11b712c69edec25e8288c3c252f3e2ff
VeerViswajit <veerviswajit2004@gmail.com>
2024-06-29 22:53:40 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -20,12 +20,16 @@ def nada_main():
     r1 = max(my_int1, my_int2)
     r6 = min(my_int1, my_int2)
     r5 = Integer(0)
+    r7 = (my_int1 - my_int2)*(my_int1*my_int1 + my_int1*my_int2 + my_int2*my_int2)
+    r8 = (my_int1 + my_int2)*(my_int1*my_int1 - my_int1*my_int2 + my_int2*my_int2)
 
     out1 = Output(r1, "Maximum of two Numbers", party1)
     out2 = Output(r, "Multiplication of Two Numbers", party1)
     out3 = Output(r3, "Division of Two Numbers", party1)
     out4 = Output(r6, "Minimum of two Numbers", party1)
     out5 = Output(r_mod, "Modulo of Two Numbers", party1)
+    out7 = Output(r7, "Difference of cubes", party1)
+    out8 = Output(r8, "Sum of cubes", party1)
     out6 = Output(r5, "Welcome to Hacker House GOA", party1)
 
-    return [out1, out2, out3, out4, out5, out6]
+    return [out1, out2, out3, out4, out5, out7, out8, out6]
